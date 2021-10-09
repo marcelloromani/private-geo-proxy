fca76ce38014790ce4a66ee339ca1bd7e5507367
marcelloromani <marcello.romani@smartpension.co.uk>
2021-10-09 11:03:46 +0100

    DRY terminate proxy lambda

--- lambdas/terminate_proxy_ec2/lambda_handler.py
+++ lambdas/terminate_proxy_ec2/lambda_handler.py
@@ -1,61 +1,12 @@
 import logging
-import os
 
-import boto3
-
-from common.constants import TAG_NAME, TAG_VALUE
+from common.ec2 import get_boto3_ec2_client, find_instances
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# The boto3 EC2 client
 _client = None
-
-
-def get_boto3_ec2_client():
-    global _client
-    if _client is not None:
-        return _client
-
-    aws_region = os.getenv('AWS_REGION')
-    if aws_region:
-        _client = boto3.client('ec2', region_name=aws_region)
-    else:
-        _client = boto3.client('ec2')
-    return _client
-
-
-def find_instances():
-    client = get_boto3_ec2_client()
-
-    filters = [
-        {
-            'Name': f'tag:{TAG_NAME}',
-            'Values': [
-                TAG_VALUE,
-            ]
-        },
-        {
-            'Name': 'instance-state-name',
-            'Values': [
-                'running',
-            ]
-        },
-    ]
-
-    response = client.describe_instances(Filters=filters)
-
-    res_count = len(response['Reservations'])
-    if res_count == 0:
-        print('No instances found with provided search parameters')
-        return []
-
-    res = response['Reservations'][0]
-    if 'Instances' not in res:
-        print('No instances found with provided search parameters')
-        return []
-
-    instances = res['Instances']
-    return instances
 
 
 def terminate_instances(instances):
@@ -69,13 +20,16 @@ def terminate_instances(instances):
 def lambda_handler(event, _):
     logger.info(event)
 
-    instances = find_instances()
+    global _client
+    if _client is None:
+        _client = get_boto3_ec2_client()
+
+    instances = find_instances(_client)
     if len(instances) == 0:
         msg = "No running instance(s) ==> Nothing to do"
         logger.info(msg)
         return msg
 
-    instances = find_instances()
     instance_ids = list(map(lambda x: x['InstanceId'], instances))
     resp = terminate_instances(instance_ids)
     logger.debug(resp)
